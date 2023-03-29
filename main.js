814fd9ef46908511bed3f9fdf72e341d88cedb7a
lineiika <avramenko.liana@gmail.com>
2023-03-29 21:49:14 +0300

    final commit

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const cnahgeImage = function (){
- let rand = Math.ceil(Math.random() * 9);
- document.querySelector('img').src = 'images/' + rand + '.png'
-} 
 
-cnahgeImage();
+const btnLink = document.querySelector(".btnLink");
+const btnPromtLink = document.querySelector(".btnPrompt");
+let link;
 
-document.querySelector('button').addEventListener('click', cnahgeImage);
+btnPromtLink.addEventListener('click', function () {
+    link = prompt("Enter your link");
+    if (link.startsWith('http') === false && link.startsWith('https') === false) {
+        link = `https://${link}`;
+    }
+    location.href = link;
+});
 
+btnLink.addEventListener("click", function (e) {
+    let btnUrl = e.target.getAttribute('data-url');
 
-
-
-
-
-
- 
+    if (btnUrl.startsWith('http') === false && btnUrl.startsWith('https') === false) {
+        btnUrl= `https://${btnUrl}`;
+    }
+    location.href = btnUrl;
+})
